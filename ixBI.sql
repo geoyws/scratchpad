e43606a5824ea8871c3c8e22a308c4b6936b770c
George Yong Wen Soong <geoyws@gmail.com>
2024-06-28 18:10:50 +0800

    'quick update'

--- ixBI.sql
+++ ixBI.sql
@@ -9,7 +9,7 @@ WITH sales_data AS (
         SUM(CASE WHEN "salesType" SIMILAR TO 'SaaS - New' THEN "actualAmount" ELSE 0 END) AS "npsSaaS",
         SUM(CASE WHEN "salesType" SIMILAR TO 'Out Right - New' THEN "actualAmount" ELSE 0 END) AS "npsOutright",
         SUM(CASE WHEN "salesType" SIMILAR TO 'Post Sales' THEN "actualAmount" ELSE 0 END) AS "ps",
-        SUM(CASE WHEN "salesType" SIMILAR TO '(SaaS - Services|SSA)' THEN "actualAmount" ELSE 0 END) AS "rs",
+        SUM(CASE WHEN "salesType" SIMILAR TO '(SaaS - Services|SSA)' THEN "actualAmount" ELSE 0 END) AS "r",
         SUM(CASE WHEN "salesType" SIMILAR TO '(SaaS - Services)' THEN "actualAmount" ELSE 0 END) AS "rsSaaS",
         SUM(CASE WHEN "salesType" SIMILAR TO '(SSA)' THEN "actualAmount" ELSE 0 END) AS "rsSSA"
     FROM
@@ -22,26 +22,187 @@ WITH sales_data AS (
         "market", "businessUnit", "year", "quarter", "month"
 )
 SELECT
-  'Total Sales Order' as 'scope',
-  SUM(CASE WHEN "quarter" = 1 THEN SUM("nps", "ps", "recurring")) AS "Q1",
-  SUM(CASE WHEN "month" = 1 THEN SUM("nps", "ps", "recurring")) AS "Jan",
-  SUM(CASE WHEN "month" = 2 THEN SUM("nps", "ps", "recurring")) AS "Feb",
-  SUM(CASE WHEN "month" = 3 THEN SUM("nps", "ps", "recurring")) AS "Mar",
-  SUM(CASE WHEN "quarter" = 2 THEN SUM("nps", "ps", "recurring")) AS "Q2",
-  SUM(CASE WHEN "month" = 4 THEN SUM("nps", "ps", "recurring")) AS "Apr",
-  SUM(CASE WHEN "month" = 5 THEN SUM("nps", "ps", "recurring")) AS "May",
-  SUM(CASE WHEN "month" = 6 THEN SUM("nps", "ps", "recurring")) AS "Jun",
-  SUM(CASE WHEN "quarter" = 3 THEN SUM("nps", "ps", "recurring")) AS "Q3",
-  SUM(CASE WHEN "month" = 7 THEN SUM("nps", "ps", "recurring")) AS "Jul",
-  SUM(CASE WHEN "month" = 8 THEN SUM("nps", "ps", "recurring")) AS "Aug",
-  SUM(CASE WHEN "month" = 9 THEN SUM("nps", "ps", "recurring")) AS "Sep",
-  SUM(CASE WHEN "quarter" = 4 THEN SUM("nps", "ps", "recurring")) AS "Q4",
-  SUM(CASE WHEN "month" = 10 THEN SUM("nps", "ps", "recurring")) AS "Oct",
-  SUM(CASE WHEN "month" = 11 THEN SUM("nps", "ps", "recurring")) AS "Nov",
-  SUM(CASE WHEN "month" = 12 THEN SUM("nps", "ps", "recurring")) AS "Dec",
+  'Total Sales Order' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
 FROM
   sales_data
-GROUP BY
-    "market", "businessUnit", "year", "quarter", "month"
+UNION ALL
+SELECT
+  'Malaysia' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "market" = 'Malaysia'
+--  HQ-Property 
+--  HQ-Construction 
+--  HQ-Hotel 
+--  HQ-HR 
+--  Northern 
+--  Southern 
+--  East Coast 
+--  EM-Sabah  
+--  EM-Sarawak 
+--  Gotta UNION ALL for the above scopes
+UNION ALL
+SELECT
+  'HQ-Property' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "businessUnit" = 'HQ-Property'
+AND
+  "market" = 'Malaysia'
+UNION ALL
+SELECT
+  'HQ-Construction' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "businessUnit" = 'HQ-Construction'
+AND
+  "market" = 'Malaysia'
+UNION ALL
+SELECT
+  'HQ-Hotel' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "businessUnit" = 'HQ-Hotel'
+AND
+  "market" = 'Malaysia'
+UNION ALL
+SELECT
+  'HQ-HR' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "businessUnit" = 'HQ-HR'
+AND
+  "market" = 'Malaysia'
+UNION ALL
+SELECT
+  'Northern' as "scope",
+  SUM(CASE WHEN "quarter" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q1",
+  SUM(CASE WHEN "month" = 1 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jan",
+  SUM(CASE WHEN "month" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Feb",
+  SUM(CASE WHEN "month" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Mar",
+  SUM(CASE WHEN "quarter" = 2 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q2",
+  SUM(CASE WHEN "month" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Apr",
+  SUM(CASE WHEN "month" = 5 THEN "nps" + "ps" + "r" ELSE 0 END) AS "May",
+  SUM(CASE WHEN "month" = 6 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jun",
+  SUM(CASE WHEN "quarter" = 3 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q3",
+  SUM(CASE WHEN "month" = 7 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Jul",
+  SUM(CASE WHEN "month" = 8 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Aug",
+  SUM(CASE WHEN "month" = 9 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Sep",
+  SUM(CASE WHEN "quarter" = 4 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Q4",
+  SUM(CASE WHEN "month" = 10 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Oct",
+  SUM(CASE WHEN "month" = 11 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Nov",
+  SUM(CASE WHEN "month" = 12 THEN "nps" + "ps" + "r" ELSE 0 END) AS "Dec"
+FROM
+  sales_data
+WHERE
+  "businessUnit" = 'Northern'
+AND
+  "market" = 'Malaysia'
+UNION ALL
+
+  
+
+
 
 
